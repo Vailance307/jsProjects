1e0d8dc520035210656866ee9988fa021ae89d01
Vailance307 <vailancemkanjumwa@gmail.com>
2024-02-14 09:53:29 +0300

    additions

--- memoryGame/app.js
+++ memoryGame/app.js
@@ -1,6 +1,11 @@
-const cardArray = [
-    {
-        name: 'fries',
-        img: 
-    }
-]
+const story = document.body.querySelector(".story");
+
+const setText = document.body.querySelector("#set-text");
+setText.addEventListener("click", () => {
+  story.textContent = "It was a dark and stormy night...";
+});
+
+const clearText = document.body.querySelector("#clear-text");
+clearText.addEventListener("click", () => {
+  story.textContent = "";
+});
